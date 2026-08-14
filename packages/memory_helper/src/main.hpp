04b1e4d35503d5248105a1ae003c5daad4568ac5
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2026-08-14 22:52:18 +0100

    Update main.hpp

--- packages/memory_helper/src/main.hpp
+++ packages/memory_helper/src/main.hpp
@@ -1,5 +1,6 @@
 #pragma once
-#include "funcs.h"
+#pragma once
+#include <Windows.h>
 
 #if defined(_WIN32) || defined(_WIN64)
     #define MEMORY_API __declspec(dllexport)
@@ -12,10 +13,26 @@ extern "C"
 {
 #endif
 
-    MEMORY_API HANDLE open_process(DWORD process_id);
-    MEMORY_API funcs::Array collect_regions(HANDLE hProcess);
-    MEMORY_API char*        disassemble_region(HANDLE hProcess, SIZE_T size, ULONG_PTR start_address);
-    MEMORY_API void         patch_bytes(HANDLE hProcess, LPVOID lpAddress, const char* bytes, size_t size);
+    typedef struct
+    {
+        char*  data;
+        size_t size;
+        int    len;
+    } Array;
+
+    typedef struct
+    {
+        const char*    asmcode;
+        void*          subKeys;
+        void*          subValues;
+        size_t         subCount;
+    } C_FunctionMap;
+
+    MEMORY_API HANDLE        open_process(DWORD process_id);
+    MEMORY_API Array         collect_regions(HANDLE hProcess);
+    MEMORY_API char*         disassemble_region(HANDLE hProcess, size_t size, ULONG_PTR start_address);
+    MEMORY_API void          patch_bytes(HANDLE hProcess, void* lpAddress, const char* bytes, size_t size);
+    MEMORY_API C_FunctionMap* build_funcmap(HANDLE hProcess);
 
 #ifdef __cplusplus
 }
