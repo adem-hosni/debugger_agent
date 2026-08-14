1ff05b44c67a138840db8da51bebb7640ce2235d
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2026-08-14 22:52:23 +0100

    Update main.cpp

--- packages/memory_helper/src/main.cpp
+++ packages/memory_helper/src/main.cpp
@@ -1,24 +1,39 @@
 #include "main.hpp"
+#include "main.hpp"
+#include "funcs.h"
+#include "funcmap_builder.h"
+
+extern "C" {
 
 MEMORY_API HANDLE open_process(DWORD process_id)
 {
     return funcs::open_process(process_id);
 }
 
-MEMORY_API funcs::Array collect_regions(HANDLE hProcess)
+MEMORY_API Array collect_regions(HANDLE hProcess)
 {
-    return funcs::collect_regions(hProcess);
+    funcs::Array result = funcs::collect_regions(hProcess);
+    return {result.data, result.size, result.len};
 }
 
-MEMORY_API char* disassemble_region(HANDLE hProcess, SIZE_T size, ULONG_PTR address)
+MEMORY_API char* disassemble_region(HANDLE hProcess, size_t size, ULONG_PTR address)
 {
     return funcs::disassemble_region(hProcess, size, address);
 }
 
-MEMORY_API void patch_bytes(HANDLE hProcess, LPVOID lpAddress, const char* bytes, size_t size)
+MEMORY_API void patch_bytes(HANDLE hProcess, void* lpAddress, const char* bytes, size_t size)
 {
-    return funcs::patch_bytes(hProcess, lpAddress, bytes, size);
+    funcs::patch_bytes(hProcess, lpAddress, bytes, size);
 }
+
+MEMORY_API C_FunctionMap* build_funcmap(HANDLE hProcess)
+{
+    funcmap_builder::stFunctionMap funcmap = funcmap_builder::build_funcmap(hProcess);
+    funcmap_builder::C_FunctionMap* c_funcmap = funcmap_builder::ConvertToC(&funcmap);
+    return reinterpret_cast<C_FunctionMap*>(c_funcmap);
+}
+
+}  // extern "C"
 
 int main()
 {
