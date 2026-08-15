288af024479bce1d84c47011a700f386b9c9f6f1
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2026-08-15 14:07:43 +0100

    Update main.py

--- packages/agent/src/main.py
+++ packages/agent/src/main.py
@@ -5,10 +5,7 @@ def main():
     if not mem_helper.attach("level1easy.exe"):
         print(f"Failed to attach to the target process! {mem_helper.get_last_error()}")
         return
-    array = mem_helper.collect_regions()
-    print(f"Collected {array.len} regions")
-    
-    mem_helper.disassemble_regions(array)
+    print(mem_helper.build_funcmap())
 
 
 if __name__ == "__main__":
