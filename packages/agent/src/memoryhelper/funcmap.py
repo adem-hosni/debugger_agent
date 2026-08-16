fc3dc3e959281cd1f52bd5f448de630850c3ad27
H y p e r <132200124+HYPER-00@users.noreply.github.com>
2026-08-16 23:16:29 +0100

    Update funcmap.py

--- packages/agent/src/memoryhelper/funcmap.py
+++ packages/agent/src/memoryhelper/funcmap.py
@@ -4,22 +4,31 @@ from typing import Any, Self
 
 class FunctionNode:
     def __init__(self, offset: str, asm_code: str):
-        self.offset = offset
+        self.offset = offset.lower()
         self.asm_code = asm_code
         self.inner_nodes: set[FunctionNode] = set()
 
     def find_node(self, offset: str) -> Self | None:
+        offset = f"{int(offset, base=16):x}"
         if offset == self.offset or offset == "0x" + self.offset:
             return self
         for inner_node in self.inner_nodes:
             if inner_node.find_node(offset):
                 return inner_node
+        return self.find_node_by_instr_offset(offset)
+
+    def find_node_by_instr_offset(self, offset: str):
+        if self.asm_code.lower().find(offset.lower() + "  "):
+            return self
+        for inner_node in self.inner_nodes:
+            if inner_node.find_node_by_instr_offset(offset):
+                return inner_node
 
     def parse_inner_nodes(self, node_map: dict[str, Any]):
         for offset, v in node_map.get("subFunctions", {}).items():
             node = FunctionNode(offset, v["asmcode"])
-            node.parse_inner_nodes(v)
             self.inner_nodes.add(node)
+            node.parse_inner_nodes(v)
 
 
 class FunctionMap(FunctionNode):
